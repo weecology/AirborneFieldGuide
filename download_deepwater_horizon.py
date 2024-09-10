c849382ffe3957dfdaa398db537725071cd8584d
bw4sz <benweinstein2010@gmail.com>
2024-09-10 10:08:22 -0400

    add download deepwater horizon

--- download_deepwater_horizon.py
+++ download_deepwater_horizon.py
@@ -1,30 +1,25 @@
 import os
 import subprocess
+import boto3
 
-def download_images_from_s3(url, num_images):
-    # Extract bucket name and prefix from the URL
-    bucket_name = url.split('/')[2]
-    prefix = '/'.join(url.split('/')[3:])
+# Set the S3 bucket URL
+bucket_url = "s3://twi-aviandata/avian_monitoring/high_resolution_photos/"
 
-    # List all objects in the specified bucket and prefix
-    command = f"aws s3 ls s3://{bucket_name}/{prefix} --recursive"
-    output = subprocess.check_output(command, shell=True).decode('utf-8')
+# Create a Boto3 S3 client
+s3_client = boto3.client('s3')
 
-    # Create the target directory if it doesn't exist
-    target_directory = "/orange/ewhite/b.weinstein/DeepWaterHorizon/"
-    os.makedirs(target_directory, exist_ok=True)
+# List all subfolders in the S3 bucket
+subfolders = subprocess.check_output(['aws', 's3', 'ls', bucket_url]).decode('utf-8').split('\n')
+subfolders = [folder.split(' ')[-2] for folder in subfolders if folder]
 
-    # Iterate over the objects and download two images from each folder
-    count = 0
-    for line in output.splitlines():
-        if count < num_images:
-            # Extract the object key from the output line
-            object_key = line.split()[-1]
-
-            # Download the image using wget and save it in the target directory
-            command = f"wget s3://{bucket_name}/{object_key} -P {target_directory}"
-            subprocess.run(command, shell=True)
-
-            count += 1
-
-download_images_from_s3("s3://deepwaterhorizon", 2)
+# Download two images from each subfolder
+for subfolder in subfolders:
+    # List all objects in the subfolder
+    objects = s3_client.list_objects_v2(Bucket='twi-aviandata', Prefix=f"avian_monitoring/high_resolution_photos/{subfolder}/")['Contents']
+    
+    # Download the first two images
+    for i in range(2):
+        object_key = objects[i]['Key']
+        file_name = os.path.basename(object_key)
+        s3_client.download_file('twi-aviandata', object_key, file_name)
+        print(f"Downloaded {file_name} from {subfolder}")
